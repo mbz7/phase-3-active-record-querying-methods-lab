4551c2e92294fbd94544bd934a152d7e36cd7d94
Myles <bracken.myles@gmail.com>
2022-02-11 10:48:39 -0800

    commit tree 3cb5084939fc1599b023d3d31e961dc981354168

---
+++ app/models/show.rb
@@ -0,0 +1,38 @@
+class Show < ActiveRecord::Base
+
+    # .highest_rating: This method should return the highest value in the ratings column. Hint: if there is a .minimum Active Record method, might there be a .maximum method?
+    def self.highest_rating
+        self.maximum(:rating)
+    end
+
+    # .most_popular_show: Returns the show with the highest rating.
+    def self.most_popular_show
+        self.where("rating = ?", self.highest_rating).first
+    end
+
+    # .lowest_rating: Returns the lowest value in the ratings column.
+    def self.lowest_rating
+        self.minimum(:rating)
+    end
+
+    # .least_popular_show: Returns the show with the lowest rating.
+    def self.least_popular_show
+        self.where("rating = ?", self.lowest_rating).first
+    end
+
+    # .ratings_sum: Returns the sum of all of the ratings.
+    def self.ratings_sum
+        self.sum(:rating)
+    end
+
+    # .popular_shows: Returns a list of all of the shows that have a rating greater than 5. Hint: use the .where Active Record method.
+    def self.popular_shows
+        self.where("rating >= ?", 5)
+    end
+
+    # .shows_by_alphabetical_order: Returns an array of all of the shows sorted in alphabetical order according to their names. Hint: use the .order Active Record method.
+    def self.shows_by_alphabetical_order
+        self.order(:name)
+    end
+
+end
